8d686dd7452cadd9cc326490d7180e01aa8f7b3c
JuliaCSan <75736421+JuliaCSan@users.noreply.github.com>
2023-03-12 17:16:36 -0300

    Update Exe02.py

--- Exe02.py
+++ Exe02.py
@@ -1,25 +1,37 @@
 boletim = list()
+lista_notas1 = list()
+lista_notas2 = list()
+lista_notas3 = list()
+situacaoap = list()
+situacaorp = list()
+
 while True:
-    nome = str(input('Insira o nome do aluno: '))
+    nome = str(input('\nInsira o nome do aluno: '))
     n1 = float(input('Insina a primeira nota: '))
     n2 = float(input('Insira a segunda nota: '))
     n3 = float(input('Insira a terceira nota: '))
     media = (n1 +n2 +n3)/3
+
     print('Média: ', media)
-    boletim.append([nome, [n1, n2, n3], media])
+
+    boletim.append([nome, ['NOTA 1:'], n1, ['NOTA 2: ', n2], ['NOTA 3 :', n3], media])
+    lista_notas1.append(n1)
+    lista_notas2.append(n2)
+    lista_notas3.append(n3)
 
     if media >=7:
         print('Situação: APROVADO')
     else: print('Situação: REPROVADO')
-        
-    proximo = str(input('Deseja cadastrar o próximo aluno? [S/N]'))
+
+    proximo = str(input('\nDeseja cadastrar o próximo aluno? [S/N]'))
     if proximo in 'Nn':
-        break
-        
-print('-=' * 40)
-print(f'{"NOME":<4}{"NOTA 1":<12}{"NOTA 2":<15}{"NOTA 3":<18}{"MÉDIA":<22}')
-for i in enumerate[boletim]:
-    print(f'{nome:<4}{n1:<12}{n2:<15}{n3:<18}{media:<22}')
+        print ('=' * 40)
+        print(boletim)
+        print ('=' * 40)
+        print('AV1: ', lista_notas1)
+        print('AV2: ', lista_notas2)
+        print('AV3: ', lista_notas3)
+        print ('=' * 40)
     
 
 
